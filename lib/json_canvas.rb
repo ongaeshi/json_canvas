04ecc2ac02432b1707a5328912101abf89229e2f
ongaeshi <ongaeshi0621@gmail.com>
2024-05-02 03:37:55 +0900

    feat: Creating

--- lib/json_canvas.rb
+++ lib/json_canvas.rb
@@ -1,10 +1,43 @@
 # frozen_string_literal: true
 
 require_relative "json_canvas/version"
+require "securerandom"
 
 module JsonCanvas
-  class JsonCanvas
-    def self.from_json(json)
+  def self.create()
+    Canvas.new
+  end
+
+  def self.load(json)
+    Canvas.load(json)
+  end
+
+  class GenericNode
+    attr_reader :id, :x, :y, :width, :height, :color
+
+    def initialize(**kwargs)
+      @id = kwargs[:id] || SecureRandom.uuid.gsub('-', '')[0...16]
+      @x = kwargs[:x] || 0
+      @y = kwargs[:y] || 0
+      @width = kwargs[:width] || 10
+      @height = kwargs[:height] || 10 
+      @color = kwargs[:color] || "#f0f0f0" # Default color shoud be gray.
+    end
+  end
+
+  class TextNode < GenericNode
+    attr_reader :text
+
+    def initialize(**kwargs)
+      super(**kwargs)
+      @text = kwargs[:text] || ""
+    end
+  end
+
+  class Canvas
+    attr_reader :nodes, :edges
+
+    def self.load(json)
     end
 
     def initialize(nodes = [], edges = [])
@@ -12,25 +45,16 @@ module JsonCanvas
       @edges = edges
     end
 
-    def add_node(node)
+    def add_text(**kwargs)
+      node = TextNode.new(**kwargs)
+      nodes.push(node)
+      node
     end
 
-    def add_edge(edge)
-    end
+    # def add_edge(edge)
+    # end
 
-    def node(id)
-    end
-
-    def edge(id)
-    end
-
-    def remove_node(id)
-    end
-
-    def remove_edge(id)
-    end
-
-    def to_s
-    end
+    # def to_s
+    # end
   end
 end
--- test/json_canvas_test.rb
+++ test/json_canvas_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "json_canvas"
 
 class JsonCanvasTest < Test::Unit::TestCase
   test "VERSION" do
@@ -10,6 +11,14 @@ class JsonCanvasTest < Test::Unit::TestCase
   end
 
   test "constructor" do
-    JsonCanvas.new([], [])
+    jc = JsonCanvas.create
+    # jc = JsonCanvas::Canvas.new()
+    # jc.add_node(x: 1, y: 2, text: "Hello")
+    t = jc.add_text(x: 10, y: 0, text: "Hello")
+    p t
+    # jc.add_file()
+    # jc.add_link()
+    # g = jc.add_group()
+    # g.resize(t1, t2, t3)
   end
 end
